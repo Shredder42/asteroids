6bf4b5a0b31f17668ec6b5b8e6367b114c7c666f
shredder42 <atbinder42@gmail.com>
2024-11-11 10:31:49 -0600

    set framerate

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     pygame.init()
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,10 @@ def main():
             
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+   
 
 
 if __name__ == "__main__":
